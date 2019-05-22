25d3a55563cb09bb4c4b6d6b5fe6ab8da5651116
İbrahim acar <ibrahimacar@pop-os.localdomain>
2019-05-22 06:32:35 +0300

    Test mode edited

--- app/src/main/java/com/example/abchar/TrainTestActivities/TestQuestion.java
+++ app/src/main/java/com/example/abchar/TrainTestActivities/TestQuestion.java
@@ -1,8 +1,10 @@
 package com.example.abchar.TrainTestActivities;
 
 import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,20 +12,27 @@ import android.widget.TextView;
 import com.example.abchar.R;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Random;
 
-public class TestQuestion extends AppCompatActivity {
+public class TestQuestion extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
     private String childId;
+    private TextToSpeech tts;
+    private String questionLabel;
+    private TextView question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_question);
 
+        tts = new TextToSpeech(this, this);
+        question = (TextView) findViewById(R.id.questionLetterInfo);
+
         childId = getIntent().getStringExtra("childId");
 
-        final String questionLabel = setQuestion();
+        questionLabel = setQuestion();
 
         Button startTestButton = (Button) findViewById(R.id.StartTest);
         startTestButton.setOnClickListener(new View.OnClickListener() {
@@ -48,7 +57,7 @@ public class TestQuestion extends AppCompatActivity {
     }
 
     public String setQuestion(){
-        TextView question = (TextView) findViewById(R.id.questionLetterInfo);
+
         Random random = new Random();
         ArrayList<String> labels = new ArrayList<String>() {{
             add("A");
@@ -66,8 +75,35 @@ public class TestQuestion extends AppCompatActivity {
 
         int random_number = random.nextInt(labels.size());
         String questionLabel = labels.get(random_number);
-        String content = "Show" + questionLabel;
+        String content = "Listen and show the character you heared" ;
         question.setText(content);
         return  questionLabel;
+    }
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = tts.setLanguage(Locale.US);
+            tts.setSpeechRate(0.7f);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                speakOut();
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
+    }
+
+
+    public void speakOut() {
+        String commands = question.getText().toString();
+        String letterString = "Show me: " + questionLabel;
+        tts.speak(commands, TextToSpeech.QUEUE_FLUSH, null);
+
+
     }
 }
